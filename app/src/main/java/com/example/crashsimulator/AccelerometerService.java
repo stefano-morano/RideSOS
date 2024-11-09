d56e7706868a58a49ea8462c200f208723fd38fa
Antonio Franzoso <antonio.franzoso@outlook.it>
2024-11-09 20:49:19 +0100

    Fixed notifications.

--- app/src/main/java/com/example/crashsimulator/AccelerometerService.java
+++ app/src/main/java/com/example/crashsimulator/AccelerometerService.java
@@ -5,12 +5,14 @@ import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.Service;
 import android.content.Intent;
+import android.content.pm.ServiceInfo;
 import android.os.Build;
 import android.os.IBinder;
 import android.util.Log;
 
 import androidx.annotation.Nullable;
 import androidx.core.app.NotificationCompat;
+import androidx.core.app.ServiceCompat;
 
 public class AccelerometerService extends Service {
     private Thread readingSensorThread;
@@ -27,13 +29,29 @@ public class AccelerometerService extends Service {
         accelerometerSensor.start();
         logAccelerometerData();
 
+        // Create notification in the topBar
         createNotificationChannel();
         Notification notification = new NotificationCompat.Builder(this, CHANNEL_ID)
-                .setContentTitle("SOSRide")
-                .setContentText("SOSRide is working")
+                .setContentTitle("Crash Detection activated")
+                .setContentText("Have a safe ride!")
                 .setSmallIcon(R.drawable.ic_profile)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setForegroundServiceBehavior(NotificationCompat.FOREGROUND_SERVICE_IMMEDIATE)
                 .build();
-        startForeground(1, notification);
+
+        int type = 0;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            type = ServiceInfo.FOREGROUND_SERVICE_TYPE_SPECIAL_USE;
+        }
+
+        ServiceCompat.startForeground(
+                this,
+                /* id = */ 1, // Cannot be 0
+                /* notification = */ notification,
+                /* foregroundServiceType = */ type
+        );
+
+//        startForeground(1, notification);
         return START_STICKY;
     }
 
@@ -53,18 +71,20 @@ public class AccelerometerService extends Service {
     }
 
     private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is not in the Support Library.
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            NotificationChannel serviceChannel = new NotificationChannel(
-                    CHANNEL_ID,
-                    "Accelerometer Service Channel",
-                    NotificationManager.IMPORTANCE_DEFAULT
-            );
+            CharSequence name = getString(R.string.channel_name);
+            String description = getString(R.string.channel_description);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel serviceChannel = new NotificationChannel(CHANNEL_ID, name, importance);
+            serviceChannel.setDescription(description);
+            // Register the channel with the system. You can't change the importance
+            // or other notification behaviors after this.
             serviceChannel.setVibrationPattern(new long[]{0});
             serviceChannel.enableVibration(false);
-            NotificationManager manager = getSystemService(NotificationManager.class);
-            if (manager != null) {
-                manager.createNotificationChannel(serviceChannel);
-            }
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(serviceChannel);
         }
     }
 
